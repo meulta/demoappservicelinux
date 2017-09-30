0a054c7160675f093342a03d233ed97bb07f0708
Etienne Margraff <etmargra@microsoft.com>
2017-09-30 15:24:20 +0200

    asd

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import * as http from 'http'
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Salut tout le monde !');
+  res.end('Salut tout les gens !');
 });
 
 server.listen(80);
